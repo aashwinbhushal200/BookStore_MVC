a44713f9dbfb648b3e651ed4891dbce0b3f2c5bd
aashwinbhushal961 <aashwinbhushal961@outlook.com>
2024-06-25 15:17:59 +0530

    Categories-db-table-created

---
+++ BookStore_MVC_Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStore_MVC_Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore_MVC_Web.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
