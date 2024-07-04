262fa056b545c30f049459e507536b2a7c8b4623
aashwinbhushal961 <aashwinbhushal961@outlook.com>
2024-07-04 22:08:16 +0530

    added-newFields-to-Db

--- BookStore_MVC_Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookStore_MVC_Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -149,7 +149,9 @@ namespace BookStore_MVC_Web.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-
+                user.Name= Input.Name;
+                user.PhoneNumber= Input.PhoneNumber;
+                user.PostalCode = Input.PostalCode;
 
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
