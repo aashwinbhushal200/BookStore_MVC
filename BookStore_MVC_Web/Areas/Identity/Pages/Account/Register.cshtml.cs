80206bc0ee0284aae38e72288fdc06c8ac45d1dd
aashwinbhushal961 <aashwinbhushal961@outlook.com>
2024-07-04 22:01:52 +0530

    added-new-fields

--- BookStore_MVC_Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookStore_MVC_Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -107,6 +107,16 @@ namespace BookStore_MVC_Web.Areas.Identity.Pages.Account
             public string? Role { get;set; }
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+
+            //add more fields
+
+            [Required]
+            public string? Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set;}
+            public string? PhoneNumber { get; set; }
         }
 
 
@@ -139,6 +149,8 @@ namespace BookStore_MVC_Web.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
